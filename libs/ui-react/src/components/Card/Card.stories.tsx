9dfe3ca3bec9c64f91383d370ba1017e86f17983
Andras Ferenczi <a.ferenczi.andras@gmail.com>
2024-03-20 12:18:52 +0100

    Added a starting implementation

--- libs/ui-react/src/components/Card/Card.stories.tsx
+++ libs/ui-react/src/components/Card/Card.stories.tsx
@@ -15,4 +15,24 @@ export const Default: CardStory = {
   args: {},
 };
 
+export const ProductTeamA: CardStory = {
+  args: {
+    children: (
+      <div style={{ display: 'flex', flex: 1, width: '100%', height: '100%' }}>
+        dynamic content
+      </div>
+    ),
+  },
+};
+
+export const ProductTeamB: CardStory = {
+  args: {
+    headline: 'Lorem ipsum',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras commodo sed lorem a venenatis.',
+    img: 'https://placehold.co/800x400?text=Placeholder',
+    link: './',
+  },
+};
+
 // TODO: Add component stories
--- libs/ui-react/src/components/Card/Card.tsx
+++ libs/ui-react/src/components/Card/Card.tsx
@@ -1,18 +1,55 @@
 import styled from 'styled-components';
+import React from 'react';
 
 /* eslint-disable-next-line */
-export interface UiComponentsProps {}
+export interface UiComponentsProps {
+  children?: React.ReactNode;
+  img?: string;
+  headline?: string;
+  description?: string;
+  link?: string;
+}
 
 const StyledUiComponents = styled.div`
   border: 1px solid grey;
-  // TODO: update styles.
+  border-radius: 8px;
+  box-shadow: 0 4px 16px rgb(25 25 25 / 12%);
+  width: 300px;
+  height: 350px;
+`;
+
+const StyledHeadline = styled.h3`
+  padding: 12px 12px 0;
+`;
+
+const StyledImage = styled.img`
+  object-fit: cover;
+  max-width: 100%;
+`;
+
+const StyledParagraph = styled.p`
+  padding: 0 12px;
+`;
+
+const StyledLink = styled.a`
+  text-decoration: none;
+  color: black;
+  padding: 0 12px 12px 12px;
 `;
 
 export function Card(props: UiComponentsProps) {
   return (
     <StyledUiComponents>
-      Card Component
-      {/*TODO: Implement component(s). */}
+      {props.children ? (
+        props.children
+      ) : (
+        <>
+          <StyledImage src={props.img} alt="Card Image" />
+          <StyledHeadline>{props.headline}</StyledHeadline>
+          <StyledParagraph>{props.description}</StyledParagraph>
+          <StyledLink href={props.link}>&#8594;</StyledLink>
+        </>
+      )}
     </StyledUiComponents>
   );
 }
